59bd02e8ed94895030e505fcdc9939baa1f3755d
williamjohngardner <bill@williamjohngardner.com>
2016-07-18 22:03:10 -0400

    Late night commit

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView, CreateView, ListView, DetailView
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.models import User
 
-from app.models import Part
+from app.models import Part, Assembly
 
 
 class IndexView(TemplateView):
@@ -26,3 +26,20 @@ class PartListView(ListView):
 class PartDetailView(DetailView):
     model = Part
     queryset = Part.objects.all()
+
+
+class CreateAssemblyView(CreateView):
+    model = Assembly
+    fields = ['assembly_name', 'assembly_part_number', 'description', 'category', 'sub_category', 'supplier', 'supplier_pn', 'list_of_parts', 'list_of_assemblies', 'quantity', 'extended_price', 'notes', 'cad_file']
+    success_url = reverse_lazy("assembly_list_view")
+
+
+class AssemblyListView(ListView):
+    model = Assembly
+
+    def get_queryset(self):
+        return Assembly.objects.all()
+
+
+class AssemblyDetailView(DetailView):
+    pass
--- bill_of_materials/urls.py
+++ bill_of_materials/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from app.views import IndexView, CreatePartView, PartListView, PartDetailView
-from app.views import CreateBomView, BomListView, BomDetailView
+from app.views import CreateAssemblyView, AssemblyListView, AssemblyDetailView
 
 
 urlpatterns = [
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^create_part/$', CreatePartView.as_view(), name="create_part_view"),
     url(r'^part_list/$', PartListView.as_view(), name="part_list_view"),
     url(r'^part_list/(?P<pk>\d+)/$', PartDetailView.as_view(), name="part_detail_view"),
-    url(r'^create_bom/$', CreateBomView.as_view(), name="create_bom_view"),
-    url(r'^bom_list/$', BomListView.as_view(), name="bom_list_view"),
-    url(r'^bom_list/(?P<pk>\d+)/$', BomDetailView.as_view(), name="bom_detail_view")
+    url(r'^create_assembly/$', CreateAssemblyView.as_view(), name="create_assembly_view"),
+    url(r'^assembly_list/$', AssemblyListView.as_view(), name="assembly_list_view"),
+    url(r'^assembly_list/(?P<pk>\d+)/$', AssemblyDetailView.as_view(), name="assembly_detail_view")
 ]
